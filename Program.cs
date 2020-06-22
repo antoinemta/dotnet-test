f91f18e1aac5b5735eea95c35e8abca8e687f71c
Antoine <antoine_mta@hotmail.fr>
2020-06-22 22:46:05 +0200

    Modified main function.

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ namespace dotnet_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose a game (enter 1, 2 or 3) :");
             bool accessGame = false;
 
             while (!accessGame)
             {
+                Console.WriteLine("Choose a game (enter 1, 2 or 3) :");
                 string indexGame = Console.ReadLine();
                 if (indexGame != "1" && indexGame != "2" && indexGame != "3")
                 {
-                    Console.WriteLine("Error, enter 1, 2 or 3 :");
+                    Console.WriteLine("An error occurred, you may have probably entered a wrong character.");
                 }
                 else
                 {
