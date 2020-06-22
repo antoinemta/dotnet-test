19a9a21b7eebc950ce2d452e11b56b04b24fa29f
Antoine <antoine_mta@hotmail.fr>
2020-06-22 22:33:50 +0200

    Added condition to main function.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace dotnet_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Choose a game (enter 1, 2 or 3) :");
+            bool accessGame = false;
+
+            while (!accessGame)
+            {
+                string indexGame = Console.ReadLine();
+                if (indexGame != "1" && indexGame != "2" && indexGame != "3")
+                {
+                    Console.WriteLine("Error, enter 1, 2 or 3 :");
+                }
+                else
+                {
+                    Console.WriteLine("Ok");
+                    accessGame = true;
+                }
+            }
         }
     }
 }
